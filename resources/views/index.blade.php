c407f51f3c99bec9317554f165dec2b62dd8119a
AjafaM <149159094+theo-picar1@users.noreply.github.com>
2025-03-14 20:54:35 +0000

    Added all other cards to comic gallery with css

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -21,12 +21,62 @@
         </p>
     </div>
 
-    <div class="comic-gallery grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 px-10 py-10">
+    <div class="comic-gallery grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-25 px-10 py-10">
         <div class="comic-container">
             <img class="comic-image" src="{{ asset('images/invincible-comic2.jpeg') }}" />
             <div class="comic-info">
                 <h3 class="text-xl font-semibold">Invincible</h3>
                 <p class="text-gray-600 text-sm">Invincible is an American comic book series written by Robert Kirkman, illustrated by Cory Walker and Ryan Ottley, and published by Image Comics. Set in the Image Universe.....</p>
+            </div>
+            <div>
+                <button class="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600">View Details</button>
+            </div>
+        </div>
+        <div class="comic-container">
+            <img class="comic-image" src="{{ asset('images/walking-dead-comic1.jpeg') }}" />
+            <div class="comic-info">
+                <h3 class="text-xl font-semibold">The Walking Dead</h3>
+                <p class="text-gray-600 text-sm">The Walking Dead is an American post-apocalyptic comic book series created by writer Robert Kirkman and artist Tony Moore – who was the artist on the first six issues and cover artist for the first twenty-four – with art on the remainder of the series by Charlie Adlard. Beginning in 2003.....</p>
+            </div>
+            <div>
+                <button class="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600">View Details</button>
+            </div>
+        </div>
+        <div class="comic-container">
+            <img class="comic-image" src="{{ asset('images/watchmen-comic1.jpeg') }}" />
+            <div class="comic-info">
+                <h3 class="text-xl font-semibold">Watchmen</h3>
+                <p class="text-gray-600 text-sm">Watchmen is a comic book limited series by the British creative team of writer Alan Moore, artist Dave Gibbons, and colorist John Higgins. It was published monthly by DC Comics in 1986 and 1987 before being collected in a single-volume edition in 1987. Watchmen originated.....</p>
+            </div>
+            <div>
+                <button class="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600">View Details</button>
+            </div>
+        </div>
+        <div class="comic-container">
+            <img class="comic-image" src="{{ asset('images/marvel-comic1.jpeg') }}" />
+            <div class="comic-info">
+                <h3 class="text-xl font-semibold">Marvel's Civil War</h3>
+                <p class="text-gray-600 text-sm">"Civil War" is a 2006–07 Marvel Comics crossover event. The storyline consists of an eponymous seven-issue limited series, written by Mark Millar and penciled by Steve McNiven, and various tie-in books. The storyline builds upon.....</p>
+            </div>
+            <div>
+                <button class="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600">View Details</button>
+            </div>
+        </div>
+        <div class="comic-container">
+            <img class="comic-image" src="{{ asset('images/tech-jacket-comic1.jpeg') }}" />
+            <div class="comic-info">
+                <h3 class="text-xl font-semibold">Tech Jacket</h3>
+                <p class="text-gray-600 text-sm">Tech Jacket is an American comic book created by writer Robert Kirkman and artist E. J. Su, first published monthly by Image Comics for six issu.....</p>
+            </div>
+            <div>
+                <button class="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600">View Details</button>
+            </div>
+        </div>
+        <div class="comic-container">
+            <img class="comic-image" src="{{ asset('images/dc-comic1.jpg') }}" />
+            <div class="comic-info">
+                <h3 class="text-xl font-semibold">Injustice</h3>
+                <p class="text-gray-600 text-sm">Injustice: Gods Among Us is an American comic book series that serves as the prequel to the fighting video game of the same name. The series takes place in an alternate reality, where Superman descends into villainy following his famil.....</p>
             </div>
             <div>
                 <button class="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600">View Details</button>

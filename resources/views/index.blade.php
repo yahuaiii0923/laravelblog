15ad320c494e79c01240a1db9a7c80937b873531
yahuaiii0923 <D00275358@student.dkit.ie>
2025-03-20 15:25:16 +0000

    the home page scss

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,16 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="background-image grid grid-cols-1 m-auto">
+    <div class="background-image grid grid-cols-1 m-auto bg-blue-100">
         <div class="flex text-gray-100 pt-10">
             <div class="m-auto pt-4 pb-16 sm:m-auto w-4/5 block text-center">
                 <h1 class="sm:text-white text-5xl uppercase font-bold text-shadow-md pb-14">
-                    Do you want to become a developer?
+                    Welcome to the Jellycat Stories!
                 </h1>
                 <a
                     href="/blog"
-                    class="text-center bg-gray-50 text-gray-700 py-2 px-4 font-bold text-xl uppercase">
-                    Read More
+                    class="text-center text-white-700 py-2 px-4 font-bold text-xl rounded-full">
+                    Enjoy the Stories
                 </a>
             </div>
         </div>
@@ -18,46 +18,46 @@
 
     <div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-15 border-b border-gray-200">
         <div>
-            <img src="https://cdn.pixabay.com/photo/2014/05/03/01/03/laptop-336704_960_720.jpg" width="700" alt="">
+            <img src="https://www.jellycat.com/media/wysiwyg/jellycat-products.jpg" width="700" alt="Jellycat Collection">
         </div>
 
         <div class="m-auto sm:m-auto text-left w-4/5 block">
             <h2 class="text-3xl font-extrabold text-gray-600">
-                Struggling to be a better web developer?
+                Discover the Magic of Jellycat Toys!
             </h2>
 
             <p class="py-8 text-gray-500 text-s">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus.
+                Our Jellycat collection is perfect for every age, featuring soft, cuddly, and adorable plush toys. Explore our wide range today!
             </p>
 
             <p class="font-extrabold text-gray-600 text-s pb-9">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente magnam vero nostrum! Perferendis eos molestias porro vero. Vel alias.
+                From bunnies to bears, each Jellycat creation tells its own story of joy and comfort. Whether you're buying for yourself or a loved one, there's a toy to bring smiles and comfort.
             </p>
 
             <a
                 href="/blog"
                 class="uppercase bg-blue-500 text-gray-100 text-s font-extrabold py-3 px-8 rounded-3xl">
-                Find Out More
+                Learn More
             </a>
         </div>
     </div>
 
-    <div class="text-center p-15 bg-black text-white">
+    <div class="text-center p-15 bg-blue-800 text-white">
         <h2 class="text-2xl pb-5 text-l">
-            I'm an expert in...
+            Our Expertise
         </h2>
 
         <span class="font-extrabold block text-4xl py-1">
-            Ux Design
+            Plush Toy Design
         </span>
         <span class="font-extrabold block text-4xl py-1">
-            Project Management
+            Toy Safety Standards
         </span>
         <span class="font-extrabold block text-4xl py-1">
-            Digital Strategy
+            Custom Plush Creations
         </span>
         <span class="font-extrabold block text-4xl py-1">
-            Backend Development
+            Global Toy Distribution
         </span>
     </div>
 
@@ -67,34 +67,34 @@
         </span>
 
         <h2 class="text-4xl font-bold py-10">
-            Recent Posts
+            Latest Jellycat Adventures
         </h2>
 
         <p class="m-auto w-4/5 text-gray-500">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque exercitationem saepe enim veritatis, eos temporibus quaerat facere consectetur qui.
+            Dive into the world of Jellycat through our blog, featuring the latest collections, fun facts, and heartwarming stories from our plush family.
         </p>
     </div>
 
     <div class="sm:grid grid-cols-2 w-4/5 m-auto">
-        <div class="flex bg-yellow-700 text-gray-100 pt-10">
+        <div class="flex bg-blue-700 text-gray-100 pt-10">
             <div class="m-auto pt-4 pb-16 sm:m-auto w-4/5 block">
                 <span class="uppercase text-xs">
-                    PHP
+                    New Arrival
                 </span>
 
                 <h3 class="text-xl font-bold py-10">
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas necessitatibus dolorum error culpa laboriosam. Enim voluptas earum repudiandae consequuntur ad? Expedita labore aspernatur facilis quasi ex? Nemo hic placeat et?
+                    Meet the new Jellycat friends! From cuddly companions to adorable characters, these new plush toys are ready to steal your heart.
                 </h3>
 
                 <a
-                    href=""
+                    href="/blog"
                     class="uppercase bg-transparent border-2 border-gray-100 text-gray-100 text-xs font-extrabold py-3 px-5 rounded-3xl">
-                    Find Out More
+                    Discover Now
                 </a>
             </div>
         </div>
         <div>
-            <img src="https://cdn.pixabay.com/photo/2014/05/03/01/03/laptop-336704_960_720.jpg" alt="">
+            <img src="https://www.jellycat.com/media/wysiwyg/new-jellycat-toy.jpg" alt="New Jellycat Toy">
         </div>
     </div>
 @endsection

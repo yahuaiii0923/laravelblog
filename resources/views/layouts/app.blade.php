c9c6a5dc734e2c1b733af5b3a83a773c77627aa5
yahuaiii0923 <D00275358@student.dkit.ie>
2025-03-21 12:42:10 +0000

    added image carousell and navigation dots, modify blog view shoe css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,7 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
 
     <!-- Styles -->
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
